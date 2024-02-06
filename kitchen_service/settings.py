df3090741a37358f083a7ed7f7629097b663815d
Hryhoriy <hryhoriy.bilous@gmail.com>
2024-02-06 16:40:55 +0200

    aupdate settings and requirements.txt

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xu0ki%dhs1%6e&3)qo)lb85%r5+)^o+0-z9*9@z@d*!_o1iv%8'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-xu0ki%dhs1%6e&3)qo)lb85%r5+)^o+0-z9*9@z@d*!_o1iv%8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'kitchen.Cook'
 
-LOGIN_REDIRECT_URL = '/kitchen/'
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
