3d8bf9fb6fca9a3ad7dbbaf95cfb3150e247131f
Hryhoriy <hryhoriy.bilous@gmail.com>
2024-02-07 03:01:56 +0200

    upgrade allowed hosts

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "https://kitchen-service-lc2z.onrender.com"]
 
 
 # Application definition
